07a0ea05861ef46e2d5d7bec9e2b21292dadfb63
Group PC <cbilberube@gmail.com>
2020-01-31 11:42:44 -0500

    main.cpp undo

--- main.cpp
+++ main.cpp
@@ -12,15 +12,6 @@ using namespace std;
 
 int main() {
 	string File_Name = "mesh_ONERAM6_inv_ffd.su2";
-<<<<<<< Updated upstream
-	//Hello World!
-=======
-<<<<<<< HEAD
-	//Hello World2!
-=======
-
->>>>>>> parent of 27be833... Test_1
->>>>>>> Stashed changes
 	Reader FileContents;
 
 	FileContents.read_file(File_Name);
