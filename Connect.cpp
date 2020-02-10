ef26762bbdf455465922fc1473b87d2060b51cba
Charles <cbilberube@gmail.com>
2020-02-10 14:44:01 -0500

    Codage de connectivité
    Ajout des lignes de code pour la connectivité des zones

--- Connect.cpp
+++ Connect.cpp
@@ -21,6 +21,11 @@ void Connect_c::DataTransferExemple(Reader_c& read) {
 	cout << "test de transfert de donner entre class: nelem = " << read.nelem;
 }
 
+void Connect_c::InitializeGlobal(Reader_c& read) {
+	nnode_g = read.npoint;
+	nelem_g = read.nelem;
+}
+
 void Connect_c::Node2Elements() {
 
 }
@@ -32,6 +37,17 @@ void Connect_c::Element2Elements() {
 
 }
 
+int Connect_c::GetnelemArNode(int inode) {
+	// Retourne le nombre d'element autour d'un noeud
+	int nelno = esup2[inode] - esup2[inode - 1];
+	return nelno;
+}
+int Connect_c::GetielemArNode(int inode, int ielno) {
+	// Retourne l'index de l'element autour d'un noeud
+	// ielno	: i element around that node you want to acces	(0 based integrer)
+	int ielem = esup1[esup2[inode] + ielno];			
+	return ielem;
+}
 // ================================================= FACES CONNECTIVITY ====================================================
 void Connect_c::Compute_nface() {
 	
@@ -44,25 +60,136 @@ void Connect_c::Element2Faces() {
 }
 
 // ================================================= ZONES CONNECTIVITY ====================================================
-void Connect_c::Zone2nnode() {
+void Connect_c::InitializeLocal() {
+	zone2nelem = new int[nzone];
+	zone2nnode = new int[nzone];
+	checkzone = new int[nzone];
+	for (int izone = 0; izone < nzone; izone++) {
+		zone2nelem[izone] = 0;
+	}
 
+	elemglobal2local = new int* [nelem_g];
+	for (int ielem_g = 0; ielem_g < nelem_g; ielem_g++) {
+		elemglobal2local[ielem_g] = new int[2];
+	}
+
+	nodeglobal2local = new int* [nnode_g];
+	for (int inode_g = 0; inode_g < nnode_g; inode_g++) {
+		nodeglobal2local[inode_g] = new int[nzone];
+		for (int izone = 0; izone < nzone; izone++) {
+			nodeglobal2local[inode_g][izone] = -1;
+		}
+	}
+
+}
+void Connect_c::Zone2nnode() {
+	for (int izone = 0; izone < nzone; izone++) {
+		checkzone[izone] = 0;
+		zone2nnode[izone] = 0;
+	}
+
+	int ielem_g, izone, nelno;
+	for (int inode = 0; inode < nnode_g; inode++) {
+		nelno = GetnelemArNode(inode);
+
+		for (int ielno = 0; ielno < nelno; ielno++) {
+			ielem_g = GetielemArNode(inode, ielno);
+			izone = elem2zone[ielem_g];
+			//Verifie si on n’a pas deja compter le nœud dans la zone:
+			if (checkzone[izone] == 0) {
+				zone2nnode[izone] += 1;
+				checkzone[izone] = 1;
+			}
+		}
+		// Reset de checkzone
+		for (int izone = 0; izone < nzone; izone++) {
+			checkzone[izone] = 0;
+		}
+	}
 }
 void Connect_c::Zone2nelem() {
+	for (int ielem = 0; ielem < nelem_g; ielem++) {
+		// Jusqu'au ghostcell p-e
+		int izone = elem2zone[ielem];
+		zone2nelem[izone] += 1;
 
+	}
 }
 void Connect_c::Zone2Nodes() {
+	int* indexzone;
+	indexzone = new int[nzone];
+	for (int izone = 0; izone < nzone; izone++) {
+		checkzone[izone] = 0;
+		indexzone[izone] = 0;
+	}
 
+	int ielem_g, izone, nelno, idz;
+	for (int inode = 0; inode < nnode_g; inode++) {
+		nelno = GetnelemArNode(inode);
+
+		for (int ielno = 0; ielno < nelno; ielno++) {
+			ielem_g = GetielemArNode(inode, ielno);
+			izone = elem2zone[ielem_g];
+			//Verifie si on n’a pas deja compter le nœud dans la zone:
+			if (checkzone[izone] == 0) {
+				idz = indexzone[izone];
+				zone2node[izone][idz] = inode;
+				indexzone[izone] += 1;
+				checkzone[izone] = 1;
+			}
+		}
+		// Reset de checkzone
+		for (int izone = 0; izone < nzone; izone++) {
+			checkzone[izone] = 0;
+		}
+	}
+	delete[] indexzone;
 }
 void Connect_c::Zone2Elements() {
+	//Initialization:
+	int* indexzone; indexzone = new int[nzone];
+	zone2elem = new int*[nzone];
+	for (int izone = 0; izone < nzone; izone++) {
+		zone2elem[izone] = new int[zone2nelem[izone]];
+		indexzone[izone] = 0;
+	}
 
+	for (int ielem = 0; ielem < nelem_g; ielem++) {
+		int izone = elem2zone[ielem];
+		int idz = indexzone[izone];
+		zone2elem[izone][idz] = ielem;
+		indexzone[izone] += 1;
+	}
 }
 void Connect_c::NodeGlobal2Local() {
-
+	for (int izone = 0; izone < nzone; izone++) {
+		for (int inode = 0; inode < zone2nnode[izone]; inode++) {
+			int inode_g = zone2node[izone][inode];
+			nodeglobal2local[inode_g][izone] = inode;
+		}
+	}
 }
 void Connect_c::ElementGlobal2Local() {
-
+	for (int izone = 0; izone < nzone; izone++) {
+		for (int ielem = 0; ielem < zone2nelem[izone]; ielem++) {
+			int ielem_g = zone2elem[izone][ielem];
+			elemglobal2local[ielem_g][0] = ielem;
+			elemglobal2local[ielem_g][1] = izone;
+		}
+	}
 }
-void Connect_c::Zone2Coord() {
+void Connect_c::Zone2Coord(Reader_c& read) {
+	zone2coord = new double** [nzone];
+	for (int izone = 0; izone < nzone; izone++) {
+		zone2coord[izone] = new double* [zone2nnode[izone]];
+		for (int inode = 0; inode < zone2nnode[izone]; inode++) {
+			zone2coord[izone][inode] = new double[ndime];
+			int inode_g = zone2node[izone][inode];
+			for (int iD = 0; iD < ndime; iD++) {
+				zone2coord[izone][inode][iD] = read.coord[inode_g][iD];
+			}
+		}
+	}
 
 }
 void Connect_c::Element2Nodes() {
--- Connect.h
+++ Connect.h
@@ -14,16 +14,26 @@ class Reader_c;
 
 class Connect_c
 {
+private:
+	// Exemple d'entrer pour tester le code :
+	int elem2zone[16] = { 0,0,1,1,0,0,1,1,2,2,3,3,2,2,3,3 };
+	int nzone = 4;
+	int ndime = 3;
+
 public:
 	// ================================================= INITIALIZATION ====================================================
 	// (Mettre les variables a initialiser ici)
 
+
+
+
 	// Integrer :
-
+	
 	// 1D Array of Integrer :
-
+	int* esup2; int* esup1; 
+	int* zone2nelem; int* zone2nnode; int* checkzone;
 	// 2D Array of Integrer :
-
+	int** zone2node; int** zone2elem; int** elemglobal2local; int** nodeglobal2local;
 	// 3D Array of Integrer :
 
 	// Double :
@@ -31,9 +41,9 @@ public:
 	// 1D Array of Double :
 
 	// 2D Array of Double :
-
+	
 	// 3D Array of Double :
-
+	double*** zone2coord;
 
 	// =========================================== CONNECTIVITY FUNCTION MEMBERS ============================================
 	// ELEMENTS CONNECTIVITY:
@@ -41,20 +51,22 @@ public:
 	void Node2Elements();
 	void Node2Nodes();
 	void Element2Elements();
-
+	int GetnelemArNode(int inode);
+	int GetielemArNode(int inode, int ielno);
 	// FACES CONNECTIVITY:
 	void Compute_nface();
 	void Face2ElementsNodes();
 	void Element2Faces();
 
 	// ZONES CONNECTIVITY:
-	void Zone2nnode();
-	void Zone2nelem();
-	void Zone2Nodes();
-	void Zone2Elements();
+	void InitializeLocal(Reader_c& read);
+	void Zone2nnode(Reader_c& read);
+	void Zone2nelem(Reader_c& read);
+	void Zone2Nodes(Reader_c& read);
+	void Zone2Elements(Reader_c& read);
 	void NodeGlobal2Local();
 	void ElementGlobal2Local();
-	void Zone2Coord();
+	void Zone2Coord(Reader_c& read);
 	void Element2Nodes();
 	void Zone2Zones();
 
